ab662701e304afaa4696ad4fa0c456c7d6b80d2f
DIegs73 <31748058+DIegs73@users.noreply.github.com>
2022-06-02 15:48:21 +0200

    Add files via upload

---
+++ src/components/CardList/CardList.js
@@ -0,0 +1,23 @@
+import Card from "../Card/Card";
+
+
+const CardList= ({robots}) => {
+  // if(true){
+  //   throw new Error('Nooooooo')
+  // }
+
+    const cardComponent = robots.map((user, i) => {
+        return <Card 
+             key={i} 
+             id={robots[i].id} 
+             name={robots[i].name} 
+             email={robots[i].email}/>
+    });
+    return(
+      <div>
+            {cardComponent}
+      </div>
+    )
+}
+
+export default CardList;
